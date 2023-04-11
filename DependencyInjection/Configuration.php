e6d0b99dbbcd45ba08eb3f5cbefe430de41f9c7e
Marco Meyer <marco.meyer@glitchr.io>
2023-04-11 10:54:56 +0900

    Initial bundle structure

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Git\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $this->treeBuilder = new TreeBuilder('git');
+
+        $rootNode = $this->treeBuilder->getRootNode();
+        $this->addGlobalOptionsSection($rootNode);
+
+        return $this->treeBuilder;
+    }
+
+    private $treeBuilder;
+    public function getTreeBuilder(): TreeBuilder
+    {
+        return $this->treeBuilder;
+    }
+
+    private function addGlobalOptionsSection(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode; // todo..
+    }
+}
